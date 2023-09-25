3d06a62cf51b58ea40209875627c3cb663d068b8
LironeFitoussi <lironefit@icloud.com>
2023-09-25 22:16:23 +0300

    onout set

--- main.js
+++ main.js
@@ -1,25 +1,49 @@
-console.log("test");
-const shareBtn = document.querySelector("#shareBtn")
-shareBtn.addEventListener("mouseover", loadShare)
+const shareBtn = document.querySelector("#shareBtn");
+let isLoaded = false;
 
-const shareCome = `
-    <div id="shareMobileActive">
-        <span>SHARE</span>
-        <span><img src="./images/icon-facebook.svg" alt="icon-facebook"></span>
-        <span><img src="./images/icon-twitter.svg" alt="icon-twitter"></span>
-        <span><img src="./images/icon-pinterest.svg" alt="icon-pinterest"></span>
-        <span class="shareBtnActive"><img src="./images/icon-share.svg" alt="share button"></span>
-    </div>
-`;
-
-let isLoaded = false
 function loadShare() {
     const mainElem = document.querySelector("main");
+    let topPosition = -50;
+    
     if (!isLoaded) {
-        isLoaded = true
-        mainElem.innerHTML += shareCome
+        isLoaded = true;
+
+        const shareComp = document.createElement("div");
+        shareComp.id = "shareComp";
+        shareComp.className = "shareMobileActive";
+        shareComp.style.position = "absolute";
+        shareComp.style.display = "none"
+
+        const shareContent = `
+            <span>SHARE</span>
+            <span><img src="./images/icon-facebook.svg" alt="icon-facebook"></span>
+            <span><img src="./images/icon-twitter.svg" alt="icon-twitter"></span>
+            <span><img src="./images/icon-pinterest.svg" alt="icon-pinterest"></span>
+            <span class="shareBtnActive"><img src="./images/icon-share.svg" alt="share button"></span>
+        `;
+
+        shareComp.innerHTML = shareContent;
+        mainElem.appendChild(shareComp);
+        
+        const slideUp = setInterval(() => {
+            topPosition++;
+            shareComp.style.bottom = topPosition + "px";
+            shareComp.style.display = "flex"
+
+            if (topPosition == 0) {
+                clearInterval(slideUp);
+            }
+        }, 5);
     } else {
-        isLoaded = false
-        shareMobileActive.remove()
+        isLoaded = false;
+        const shareComp = document.querySelector("#shareComp");
+        if (shareComp) {
+            mainElem.removeChild(shareComp);
+        }
     }
 }
+
+
+
+shareBtn.addEventListener("mouseover", loadShare);
+shareBtn.addEventListener("mouseout", loadShare);
